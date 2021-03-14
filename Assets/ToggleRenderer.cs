1332d104e4f0a755891ac2c94e3f9229da0f22b2
William Linzan <williamlinzan@gmail.com>
2021-03-14 11:16:48 -0400

    Guitar AR/VR files pushed

---
+++ Assets/ToggleRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleRenderer : MonoBehaviour
+{
+
+    //echoAR echo;
+
+    public void VisibilityOn() {
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        if(rend.enabled)
+            rend.enabled = false;
+        else
+            rend.enabled = true;
+    }
+
+    public void VisibilityOff() {
+
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+    
+}
---
+++ Assets/hide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hide : MonoBehaviour
+{
+
+    //echoAR echo;
+
+    void Start(){
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+    
+}
